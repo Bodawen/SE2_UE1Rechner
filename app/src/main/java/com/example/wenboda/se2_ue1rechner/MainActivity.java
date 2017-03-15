018ce8a99afd00f46d01f46a28c5cb8a7d5f3ff7
Bodawen <qwertyhotwen@gmail.com>
2017-03-15 18:46:44 +0100

    Fertig

--- app/src/main/java/com/example/wenboda/se2_ue1rechner/MainActivity.java
+++ app/src/main/java/com/example/wenboda/se2_ue1rechner/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.wenboda.se2_ue1rechner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.id.edit;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick  (View v){
+
+        EditText Dd = (EditText) findViewById(R.id.editText3);
+        EditText Ds = (EditText) findViewById(R.id.editText4);
+        TextView Rel = (TextView) findViewById(R.id.textView);
+        double Dividend  =  Double.parseDouble(Dd.getText().toString());
+        double Dividsor = Double.parseDouble(Ds.getText().toString());
+        try{
+            double result = Dividend/Dividsor;
+            Rel.setText(String.valueOf(result));
+        }catch (Exception e){
+            Rel.setText("Divisor kann nicht 0 sein");
+        }
+
+
     }
 }
